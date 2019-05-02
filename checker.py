8f11e5582c8a879ffc6be95db8ffbd6865f2946c
Meher Shashwat Nigam <meher.shashwat@students.iiit.ac.in>
2019-05-02 12:56:06 +0530

    latest

--- checker.py
+++ checker.py
@@ -1,10 +1,14 @@
 f = open('20171066_20171062.txt', "r")
+right = 0
+total = 0
 for x in f:
+    total += 1
     s = x.split(' ')
     frame = s[0].split('_')
     slide = s[1].split('_')
     if(slide[0] == frame[0] and slide[1] == frame[1]):
-        continue
+        right += 1
     else:
         print(x)
         print("wrong")
+print(right, "/", total)
